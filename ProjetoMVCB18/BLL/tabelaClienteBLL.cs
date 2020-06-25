5c577b5356c9b7b2a3d68d88d32fd20dca53c7ed
Isabeldmlara <21800243@aluno.cotemig.com.br>
2020-06-25 14:49:29 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMVCB18/BLL/tabelaClienteBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using ProjetoMVCB18.DAL;
+
+namespace ProjetoMVCB18.BLL
+{
+    public class tabelaClienteBLL
+    {
+        private ConexaoDAL database = new ConexaoDAL();
+
+        public Boolean Auth(string email, string senha)
+        {
+            string consult = string.Format($@"select * from tblCliente where email = '{email}' and senha = '{senha}'");
+            DataTable dt = database.executeConsult(consult);
+            if (dt.Rows.Count == 1) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
